2027ed9d9827ce2c5453ed011a656aa2cc5eb547
Pierre Filliolaud <pierre.filliolaud@ormex.io>
2025-12-26 16:02:17 +0100

    initial revision

---
+++ src/main/java/net/filecode/ai/agent/SpringAiAgentSampleApplication.java
@@ -0,0 +1,13 @@
+package net.filecode.ai.agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiAgentSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiAgentSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/filecode/ai/agent/SpringAiAgentSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package net.filecode.ai.agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiAgentSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
